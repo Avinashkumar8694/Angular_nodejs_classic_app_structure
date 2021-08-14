9a61634a6e9e47aeeb0897a219f1e01c3d4ba9fd
Avinash Kumar Gupta <avinash.gupta@neutrinos.co>
2021-08-14 22:30:22 +0530

    ctrated instagram service

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,9 @@
 //CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-instagram-SSD_SERVICE_ID_sd_bxILd8eQ1qmBzsqw
+import { instagram as SSD_SERVICE_ID_sd_bxILd8eQ1qmBzsqw } from './sd-services/instagram';
 
 export const UserRoutes = [
     //CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY
+//CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY-instagram-SSD_SERVICE_ID_sd_bxILd8eQ1qmBzsqw
+SSD_SERVICE_ID_sd_bxILd8eQ1qmBzsqw,
 ];
